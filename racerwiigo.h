4ad01f7ffedf7bd79fd1cccf930ae8318b87d550
Matthew Zimmerman <mzimmerman@gmail.com>
2014-03-12 09:32:32 -0400

    removed wiimote code and will record time when the bib is linked to the racer Utilizing two data entry persons entering each bib # as it crosses the line minimizes data entry snafus and combats bandits We sacrifice individual time accuracy for "bunch" racers, but drastically increase result accuracy

--- racerwiigo.h
+++
@@ -1,9 +0,0 @@
-#ifndef RACERWIIGO_H
-#define RACERWIIGO_H
-
-#include <cwiid.h>
-
-extern cwiid_mesg_callback_t* getCwiidCallback();
-extern cwiid_err_t* getErrCallback();
-
-#endif // RACERWIIGO_H

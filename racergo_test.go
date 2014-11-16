be7ce3f2d0a8c320da09f3cd81e802cf599ece45
Matthew Zimmerman <mzimmerman@gmail.com>
2014-11-16 13:12:49 -0500

    Removing prettytest

--- racergo_test.go
+++ racergo_test.go
@@ -1,27 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"github.com/remogatto/prettytest"
-
 	"net/http"
 	"net/http/httptest"
 	"strconv"
 	"testing"
 	"time"
 )
-
-type testSuite struct {
-	prettytest.Suite
-}
-
-func TestRunner(t *testing.T) {
-	prettytest.RunWithFormatter(
-		t,
-		new(prettytest.TDDFormatter),
-		new(testSuite),
-	)
-}
 
 func startRace() {
 	reset()
@@ -30,53 +15,90 @@ func startRace() {
 	startHandler(w, r)
 }
 
-func (t *testSuite) TestLoadRacers() {
+func TestLoadRacers(t *testing.T) {
 	startRace()
 	// race is started, load the racers
 	req, err := uploadFile("test_runners.csv")
-	t.Nil(err)
-	t.Not(t.Nil(req))
+	if err != nil {
+		t.Errorf("Unexpected error - %v", err)
+	}
+	if req == nil {
+		t.Fatalf("Unexpected nil request")
+	}
 	w := httptest.NewRecorder()
 	uploadRacers(w, req)
-	t.Equal(w.Code, 301)
+	if w.Code != 301 {
+		t.Errorf("Expected redirect, got %d", w.Code)
+	}
 	mutex.Lock()
-	t.Equal(len(bibbedEntries), 8)
-	t.Equal(len(unbibbedEntries), 0)
-	t.Equal("G", bibbedEntries[4].Fname)
-	t.Equal("H", bibbedEntries[4].Lname)
+	if len(bibbedEntries) != 8 {
+		t.Errorf("Expected 8 bibbed entries, got %d", len(bibbedEntries))
+	}
+	if len(unbibbedEntries) != 0 {
+		t.Errorf("Expected 0 unbibbed entries, got %d", len(unbibbedEntries))
+	}
+	if bibbedEntries[4].Fname != "G" || bibbedEntries[4].Lname != "H" {
+		t.Errorf("Expected G H as 4th indexed entry, got %s %s", bibbedEntries[4].Fname, bibbedEntries[4].Lname)
+	}
 	mutex.Unlock()
 
 	req, err = uploadFile("test_runners2.csv")
-	t.Nil(err)
-	t.Not(t.Nil(req))
+	if err != nil {
+		t.Errorf("Unexpected error - %v", err)
+	}
+	if req == nil {
+		t.Fatalf("Unexpected nil request")
+	}
 	w = httptest.NewRecorder()
 	uploadRacers(w, req)
-	t.Equal(w.Code, 301)
+	if w.Code != 301 {
+		t.Errorf("Expected redirect, got %d", w.Code)
+	}
 	mutex.Lock()
-	t.Equal(len(bibbedEntries), 4)
-	t.Equal(len(unbibbedEntries), 4)
+	if len(bibbedEntries) != 4 {
+		t.Errorf("Expected 4 bibbed entries, got %d", len(bibbedEntries))
+	}
+	if len(unbibbedEntries) != 4 {
+		t.Errorf("Expected 4 unbibbed entries, got %d", len(unbibbedEntries))
+	}
 	mutex.Unlock()
 	req, err = uploadFile("test_runners3.csv")
-	t.Nil(err)
-	t.Not(t.Nil(req))
+	if err != nil {
+		t.Errorf("Unexpected error - %v", err)
+	}
+	if req == nil {
+		t.Fatalf("Unexpected nil request")
+	}
 	w = httptest.NewRecorder()
 	uploadRacers(w, req)
-	t.Equal(w.Code, 301)
+	if w.Code != 301 {
+		t.Errorf("Expected redirect, got %d", w.Code)
+	}
 	mutex.Lock()
-	t.Equal(len(bibbedEntries), 0)
-	t.Equal(len(unbibbedEntries), 8)
+	if len(bibbedEntries) != 0 {
+		t.Errorf("Expected 0 bibbed entries, got %d", len(bibbedEntries))
+	}
+	if len(unbibbedEntries) != 8 {
+		t.Errorf("Expected 8 unbibbed entries, got %d", len(unbibbedEntries))
+	}
 	mutex.Unlock()
 }
 
-func (t *testSuite) TestLink() { // includes removing of racers
+func TestLink(t *testing.T) { // includes removing of racers
 	startRace()
 	// race is started, load the racers
 	req, err := uploadFile("test_runners.csv")
-	t.Nil(err)
-	t.Not(t.Nil(req))
+	if err != nil {
+		t.Errorf("Unexpected error - %v", err)
+	}
+	if req == nil {
+		t.Fatalf("Unexpected nil request")
+	}
 	w := httptest.NewRecorder()
 	uploadRacers(w, req)
-	t.Equal(w.Code, 301)
+	if w.Code != 301 {
+		t.Errorf("Expected redirect, got %d", w.Code)
+	}
 	// 8 racers, test the beginning, middle, and end
 	tableTests := []struct {
 		place     int
@@ -113,116 +135,169 @@ func (t *testSuite) TestLink() { // includes removing of racers
 		if x.remove {
 			req.Form.Set("remove", "true")
 		}
-		t.Nil(err)
+		if err != nil {
+			t.Errorf("Unexpected error - %v", err)
+		}
 		w := httptest.NewRecorder()
 		linkBib(w, req)
-		t.Equal(x.code, w.Code, fmt.Sprintf("Iteration - %d, Expected %d, got %d - %s", i, x.code, w.Code, w.Body.Bytes()))
+		if x.code != w.Code {
+			t.Errorf("Iteration - %d, Expected %d, got %d - %s", i, x.code, w.Code, w.Body.Bytes())
+		}
 		if x.bib <= 0 || x.remove {
 			continue
 		}
 		func() {
 			mutex.Lock()
 			defer mutex.Unlock()
-			t.Equal(results[x.place-1].Confirmed, x.confirmed, fmt.Sprintf("Iteration - %d, Result %v confirmed != %v", i, results[x.place-1], x.confirmed))
-			t.Equal(bibbedEntries[x.bib].Result, results[x.place-1], fmt.Sprintf("Iteration - %d, Entry %v is not in place %d", i, bibbedEntries[x.bib], x.place))
-			t.Equal(int(bibbedEntries[x.bib].Result.Place), x.place, fmt.Sprintf("Iteration - %d, Bib %d is not in place %d but place %d!", i, x.bib, x.place, bibbedEntries[x.bib].Result.Place))
+			if results[x.place-1].Confirmed != x.confirmed {
+				t.Errorf("Iteration - %d, Result %v confirmed != %v", i, results[x.place-1], x.confirmed)
+			}
+			if bibbedEntries[x.bib].Result != results[x.place-1] {
+				t.Errorf("Iteration - %d, Entry %v is not in place %d", i, bibbedEntries[x.bib], x.place)
+			}
+			if int(bibbedEntries[x.bib].Result.Place) != x.place {
+				t.Errorf("Iteration - %d, Bib %d is not in place %d but place %d!", i, x.bib, x.place, bibbedEntries[x.bib].Result.Place)
+			}
 		}()
 	}
 }
 
-func (t *testSuite) TestPrizes() {
+func TestPrizes(t *testing.T) {
 	startRace()
 	// race is started, load the racers
 	req, err := uploadFile("test_prizes.json")
-	t.Nil(err)
-	t.Not(t.Nil(req))
+	if err != nil {
+		t.Errorf("Unexpected error - %v", err)
+	}
+	if req == nil {
+		t.Fatalf("Unexpected nil request")
+	}
 	w := httptest.NewRecorder()
 	uploadPrizes(w, req)
-	t.Equal(w.Code, 301)
+	if w.Code != 301 {
+		t.Errorf("Expected redirect, got %d", w.Code)
+	}
 	mutex.Lock()
-	t.Equal(len(prizes), 26)
+	if len(prizes) != 26 {
+		t.Errorf("Expected 26 prizes, got %d", len(prizes))
+	}
 	mutex.Unlock()
 
 	req, err = uploadFile("test_runners_prizes.csv")
-	t.Nil(err)
-	t.Not(t.Nil(req))
+	if err != nil {
+		t.Errorf("Unexpected error - %v", err)
+	}
+	if req == nil {
+		t.Fatalf("Unexpected nil request")
+	}
 	w = httptest.NewRecorder()
 	uploadRacers(w, req)
-	t.Equal(w.Code, 301)
+	if w.Code != 301 {
+		t.Errorf("Expected redirect, got %d", w.Code)
+	}
 	for x := 1; x <= len(bibbedEntries); x++ {
 		req, err = http.NewRequest("post", "", nil)
+		if err != nil {
+			t.Errorf("Unexpected error - %v", err)
+		}
+		if req == nil {
+			t.Fatalf("Unexpected nil request")
+		}
 		req.ParseForm()
 		req.Form.Set("bib", strconv.Itoa(x))
-		t.Nil(err)
 		w = httptest.NewRecorder()
 		linkBib(w, req)
-		t.Equal(w.Code, 301)
+		if w.Code != 301 {
+			t.Errorf("Expected redirect, got %d", w.Code)
+		}
 		w = httptest.NewRecorder()
 		linkBib(w, req)
-		t.Equal(w.Code, 301)
+		if w.Code != 301 {
+			t.Errorf("Expected redirect, got %d", w.Code)
+		}
 	}
 	mutex.Lock()
-	t.Equal(len(prizes[0].Winners), 1) // men's overall
-	t.Equal(prizes[0].Winners[0], results[0])
-	t.Equal(len(prizes[2].Winners), 1) // men's u10
-	t.Equal(prizes[2].Winners[0], results[6])
-	t.Equal(len(prizes[4].Winners), 0)  // men's 11-15
-	t.Equal(len(prizes[6].Winners), 0)  // men's 16-20
-	t.Equal(len(prizes[8].Winners), 0)  // men's 21-25
-	t.Equal(len(prizes[10].Winners), 0) // men's 26-30
-	t.Equal(len(prizes[12].Winners), 0) // men's 31-35
-	t.Equal(len(prizes[14].Winners), 1) // men's 36-40
-	t.Equal(prizes[14].Winners[0], results[1])
-	t.Equal(len(prizes[16].Winners), 0) // men's 41-45
-	t.Equal(len(prizes[18].Winners), 0) // men's 46-50
-	t.Equal(len(prizes[20].Winners), 2) // men's 51-55
-	t.Equal(prizes[20].Winners[0], results[3])
-	t.Equal(prizes[20].Winners[1], results[4])
-	//t.Equal(prizes[20].Winners[1], results[5]) // he didn't place
-	t.Equal(len(prizes[22].Winners), 0) // men's 56-60
-	t.Equal(len(prizes[24].Winners), 0) // men's 61+
+	EqualInt(t, len(prizes[0].Winners), 1) // men's overall
+	if prizes[0].Winners[0] != results[0] {
+		t.Errorf("Wrong winner i")
+	}
+	EqualInt(t, len(prizes[2].Winners), 1) // men's u10
+	EqualResult(t, prizes[2].Winners[0], results[6])
+	EqualInt(t, len(prizes[4].Winners), 0)  // men's 11-15
+	EqualInt(t, len(prizes[6].Winners), 0)  // men's 16-20
+	EqualInt(t, len(prizes[8].Winners), 0)  // men's 21-25
+	EqualInt(t, len(prizes[10].Winners), 0) // men's 26-30
+	EqualInt(t, len(prizes[12].Winners), 0) // men's 31-35
+	EqualInt(t, len(prizes[14].Winners), 1) // men's 36-40
+	EqualResult(t, prizes[14].Winners[0], results[1])
+	EqualInt(t, len(prizes[16].Winners), 0) // men's 41-45
+	EqualInt(t, len(prizes[18].Winners), 0) // men's 46-50
+	EqualInt(t, len(prizes[20].Winners), 2) // men's 51-55
+	EqualResult(t, prizes[20].Winners[0], results[3])
+	EqualResult(t, prizes[20].Winners[1], results[4])
+	//EqualInt(t,prizes[20].Winners[1], results[5]) // he didn't place
+	EqualInt(t, len(prizes[22].Winners), 0) // men's 56-60
+	EqualInt(t, len(prizes[24].Winners), 0) // men's 61+
 
-	t.Equal(len(prizes[1].Winners), 1) // women's overall
-	t.Equal(prizes[1].Winners[0], results[2])
-	t.Equal(len(prizes[3].Winners), 1) // women's u10
-	t.Equal(prizes[3].Winners[0], results[7])
-	t.Equal(len(prizes[5].Winners), 0)  // women's 11-15
-	t.Equal(len(prizes[7].Winners), 0)  // women's 16-20
-	t.Equal(len(prizes[9].Winners), 0)  // women's 21-25
-	t.Equal(len(prizes[11].Winners), 0) // women's 26-30
-	t.Equal(len(prizes[13].Winners), 0) // women's 31-35
-	t.Equal(len(prizes[15].Winners), 0) // women's 36-40
-	t.Equal(len(prizes[17].Winners), 0) // women's 41-45
-	t.Equal(len(prizes[19].Winners), 0) // women's 46-50
-	t.Equal(len(prizes[21].Winners), 0) // women's 51-55
-	t.Equal(len(prizes[23].Winners), 0) // women's 56-60
-	t.Equal(len(prizes[25].Winners), 0) // women's 61+
+	EqualInt(t, len(prizes[1].Winners), 1) // women's overall
+	EqualResult(t, prizes[1].Winners[0], results[2])
+	EqualInt(t, len(prizes[3].Winners), 1) // women's u10
+	EqualResult(t, prizes[3].Winners[0], results[7])
+	EqualInt(t, len(prizes[5].Winners), 0)  // women's 11-15
+	EqualInt(t, len(prizes[7].Winners), 0)  // women's 16-20
+	EqualInt(t, len(prizes[9].Winners), 0)  // women's 21-25
+	EqualInt(t, len(prizes[11].Winners), 0) // women's 26-30
+	EqualInt(t, len(prizes[13].Winners), 0) // women's 31-35
+	EqualInt(t, len(prizes[15].Winners), 0) // women's 36-40
+	EqualInt(t, len(prizes[17].Winners), 0) // women's 41-45
+	EqualInt(t, len(prizes[19].Winners), 0) // women's 46-50
+	EqualInt(t, len(prizes[21].Winners), 0) // women's 51-55
+	EqualInt(t, len(prizes[23].Winners), 0) // women's 56-60
+	EqualInt(t, len(prizes[25].Winners), 0) // women's 61+
 	mutex.Unlock()
 }
 
-func (t *testSuite) TestHumanDuration() {
+func EqualInt(t *testing.T, got, expected int) {
+	if got != expected {
+		t.Errorf("Expected %d, got %d", expected, got)
+	}
+}
+
+func EqualResult(t *testing.T, got, expected *Result) {
+	if got != expected {
+		t.Errorf("Expected %v, got %v", expected, got)
+	}
+}
+
+func EqualString(t *testing.T, got, expected string) {
+	if got != expected {
+		t.Errorf("Expected %s, got %s",expected, got)
+	}
+}
+
+func TestHumanDuration(t *testing.T) {
 	duration := HumanDuration(time.Second * 120)
-	t.Equal(duration.String(), "00:02:00.00")
-	t.Equal(duration.Clock(), "00:02:00")
+	EqualString(t,duration.String(), "00:02:00.00")
+	EqualString(t,duration.Clock(), "00:02:00")
 	duration = HumanDuration(time.Second * 0)
-	t.Equal(duration.String(), "00:00:00.00")
-	t.Equal(duration.Clock(), "00:00:00")
+	EqualString(t,duration.String(), "00:00:00.00")
+	EqualString(t,duration.Clock(), "00:00:00")
 	duration = HumanDuration(time.Hour)
-	t.Equal(duration.String(), "01:00:00.00")
-	t.Equal(duration.Clock(), "01:00:00")
+	EqualString(t,duration.String(), "01:00:00.00")
+	EqualString(t,duration.Clock(), "01:00:00")
 	duration = HumanDuration(time.Second * 5)
-	t.Equal(duration.String(), "00:00:05.00")
-	t.Equal(duration.Clock(), "00:00:05")
+	EqualString(t,duration.String(), "00:00:05.00")
+	EqualString(t,duration.Clock(), "00:00:05")
 	duration = HumanDuration(time.Second * 50)
-	t.Equal(duration.String(), "00:00:50.00")
-	t.Equal(duration.Clock(), "00:00:50")
+	EqualString(t,duration.String(), "00:00:50.00")
+	EqualString(t,duration.Clock(), "00:00:50")
 	duration = HumanDuration(time.Hour + time.Minute*45 + time.Second*5)
-	t.Equal(duration.String(), "01:45:05.00")
-	t.Equal(duration.Clock(), "01:45:05")
+	EqualString(t,duration.String(), "01:45:05.00")
+	EqualString(t,duration.Clock(), "01:45:05")
 	duration = HumanDuration(time.Hour + time.Minute*45 + time.Second*5 + time.Millisecond*104)
-	t.Equal(duration.String(), "01:45:05.10")
-	t.Equal(duration.Clock(), "01:45:05")
+	EqualString(t,duration.String(), "01:45:05.10")
+	EqualString(t,duration.Clock(), "01:45:05")
 	duration = HumanDuration(time.Hour + time.Minute*45 + time.Second*5 + time.Millisecond*907)
-	t.Equal(duration.String(), "01:45:05.91")
-	t.Equal(duration.Clock(), "01:45:05")
+	EqualString(t,duration.String(), "01:45:05.91")
+	EqualString(t,duration.Clock(), "01:45:05")
 }
